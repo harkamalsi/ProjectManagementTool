e75bae784534a87368a2951731b1a28330187323
harkamalsi <harkamal_guraya@hotmail.com>
2019-06-11 22:30:26 +0200

    Application is connected to redux. Next is firebase.

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {};
+const initState = {
+  projects: [
+    {id: '1', title: 'Organize documents', content: 'Ask Per and Katja to get the important documents and organize them yourself'},
+    {id: '2', title: 'Call the client', content: 'Ask Per and Katja to get the important documents and organize them yourself'},
+    {id: '3', title: 'Inform the board members', content: 'Write an email explaining the result'}
+  ]
+};
 
 const projectReducer = (state = initState, action) => {
   return state;

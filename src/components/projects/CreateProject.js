024c76a289c5714f07b6519708df584f2183ab9d
harkamalsi <harkamal_guraya@hotmail.com>
2019-06-12 09:47:04 +0200

    React thunk.

--- src/components/projects/CreateProject.js
+++ src/components/projects/CreateProject.js
@@ -1,20 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { createProject } from "../../store/actions/projectActions";
 
 class CreateProject extends Component {
   state = {
-    title: '',
-    content: ''
-  }
+    title: "",
+    content: ""
+  };
 
   handleChange = e => {
     this.setState({
       [e.target.id]: e.target.value
-    })
-  }
+    });
+  };
 
   handleSubmit = e => {
     e.preventDefault();
-  }
+    this.props.createProject(this.state);
+  };
 
   render() {
     return (
@@ -23,19 +26,31 @@ class CreateProject extends Component {
           <h5 className="grey-text text-darken-3">Create new project</h5>
           <div className="input-field">
             <label htmlFor="title">Title</label>
-            <input type="text" id="title" onchange={this.handleChange}/>
+            <input type="text" id="title" onchange={this.handleChange} />
           </div>
           <div className="input-field">
             <label htmlFor="content">Project Content</label>
-            <textarea id="content" className="materialize-textarea" onchange={this.handleChange}></textarea>
+            <textarea
+              id="content"
+              className="materialize-textarea"
+              onchange={this.handleChange}
+            />
           </div>
           <div className="input-field">
             <button className="btn pink lighten-1 z-depth-0">Create</button>
           </div>
         </form>
       </div>
-    )
+    );
   }
 }
 
-export default CreateProject
+const mapDispatchToProps = (dispatch) => {
+  return {
+    // createProject makes the async call to database
+    createProject: (project) => dispatch(createProject(project))
+  }
+}
+
+//first parameter of connect is the state but since we dont have any state to send here, we send a null. 
+export default connect(null, mapDispatchToProps)(CreateProject);
--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -4,6 +4,7 @@ import ProjectSummary from "./ProjectSummary";
 const ProjectList = ({ projects }) => {
   return (
     <div className="project-list section">
+      
       {projects &&
         projects.map(project => {
           return <ProjectSummary project={project} key={project.id} />;

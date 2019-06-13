fcfe9e989c228f485db8f1b604258978f5e88024
harkamalsi <harkamal_guraya@hotmail.com>
2019-06-13 21:03:54 +0200

    Upstream to firebase is implemented. Next step is to get the data from the firebase database.

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -1,7 +1,7 @@
 const initState = {
   projects: [
     {id: '1', title: 'Organize documents', content: 'Ask Per and Katja to get the important documents and organize them yourself'},
-    {id: '2', title: 'Call the client', content: 'Ask Per and Katja to get the important documents and organize them yourself'},
+    {id: '2', title: 'Call the client', content: 'Ask Omer to call and fix the meeting with clients'},
     {id: '3', title: 'Inform the board members', content: 'Write an email explaining the result'}
   ]
 };
@@ -9,9 +9,14 @@ const initState = {
 const projectReducer = (state = initState, action) => {
   switch(action.type) {
     case 'CREATE_PROJECT':
-      //write here
+      console.log('created project', action.project);
+      return state;
+    case 'CREATE_PROJECT_ERROR':
+      console.log('create project error', action.err);
+      return state;
+    default:
+      return state;
   }
-  return state;
 }
 
 export default projectReducer;

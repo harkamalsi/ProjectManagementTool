617f19ecc5e24ab5e3bd5729822fe98745655066
harkamalsi <harkamal_guraya@hotmail.com>
2019-06-18 21:29:51 +0200

    Notifications server-side functions for user joined and project created are done.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,51 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+//Just to initialize
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase! This is just a test.");
+});
+
+//this function is made for resuablity purpose
+const createNotification = notification => {
+  return admin
+    .firestore()
+    .collection("notifications")
+    .add(notification)
+    .then(doc => console.log("Notification added", doc));
+};
+
+//this is a function (projectCreated). The trigger is a new document being added to the projects collection.
+exports.projectCreated = functions.firestore
+  .document("projects/{projectId}")
+  .onCreate(doc => {
+    //new notification which will notify a user that a new project was added
+    const project = doc.data();
+    const notification = {
+      content: "Added a new project",
+      user: `${project.authorFirstName} ${project.authorLastName}`,
+      time: admin.firestore.FieldValue.serverTimestamp()
+    };
+
+    return createNotification(notification);
+  });
+
+exports.userJoined = functions.auth.user().onCreate(user => {
+  //here we are finding info of the relavant user (the user which was created)
+  return admin
+    .firestore()  
+    .collection("users")
+    .doc(user.uid)
+    .get()
+    .then(doc => {
+      const newUser = doc.data();
+      const notification = {
+        content: "Joined the channel",
+        user: `${newUser.firstName} ${newUser.lastName}`,
+        time: admin.firestore.FieldValue.serverTimestamp()
+      };
+
+      return createNotification(notification);
+    });
+});
--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const Notifications = () => {
+const Notifications = (props) => {
   return (
-    <div>
-      <p>Noticifations</p>
+    <div className="section">
+      <div className="card z-depth-0">
+        <div className="card-content orange accent-1">
+          <span className="card-title">Notifications</span>
+          <ul className="notifications">
+            <li>Notification</li>
+            <li>Notification</li>
+            <li>Notification</li>
+            <li>Notification</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Notifications;
